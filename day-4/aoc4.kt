8141345c66d49f206c478bd057716a52a39a247e
vitortterra <vitortterra@users.noreply.github.com>
2021-12-09 01:47:06 -0300

    day 4 part 2

--- day-4/aoc4.kt
+++ day-4/aoc4.kt
@@ -1,75 +1,75 @@
 data class Board(
-  val id: Int, 
-  val numbers: Array<IntArray>, 
-  val isMarked: Array<BooleanArray>
+  val id: Int,
+  val cells: Array<Array<BoardCell>>,
+  var isActive: Boolean = true,
 ) 
+
+data class BoardCell(
+  val number: Int,
+  var isMarked: Boolean = false,
+)
 
 data class NumberInstance(
   val boardId: Int,
   val row: Int,
-  val column: Int,
+  val col: Int,
+)
+
+data class Winner(
+  val boardId: Int,
+  val score: Int,
 )
 
 class Game {
   var boards = HashMap<Int, Board>()
   var numberInstances = HashMap<Int, List<NumberInstance>>()
-  private var lastDrawn : Int? = null
+  var winners = mutableListOf<Winner>()
 
-  fun checkWinner() : Int? {
-    if (lastDrawn != null) {
-      numberInstances[lastDrawn]?.forEach({
-        val board = boards[it.boardId]!!
-        val nrows = board.numbers.size
-        val ncols = board.numbers[0].size
-        
-        val columnComplete = (0..(nrows-1)).all({
-          i -> board.isMarked[i][it.column]
-        })
-        val rowComplete = (0..(ncols-1)).all({  
-          j -> board.isMarked[it.row][j]
-        })
+  val isOver get() = boards.size == winners.size
 
-        if (columnComplete || rowComplete) {
-          return board.id
-        }
-      })
-    }
+  fun checkWinner(boardId: Int, row: Int, col: Int) : Int? {
+    val board = boards[boardId]!!
+    val nrows = board.cells.size
+    val ncols = board.cells[0].size
+    
+    val colComplete = (0..(nrows-1)).all({
+      i -> board.cells[i][col].isMarked
+    })
+    val rowComplete = (0..(ncols-1)).all({  
+      j -> board.cells[row][j].isMarked
+    })
+    if (colComplete || rowComplete) {
+      return board.id
+    } 
+
     return null
   }
 
-  fun getScore(boardId: Int) : Int {
-    val board = boards[boardId]
-    val ld = lastDrawn
-    var unmarkedSum = 0
+  fun drawNumber(number: Int) {
+    numberInstances[number]?.forEach({
+      val (boardId, row, col) = it
+      val board = boards[boardId]!!
 
-    if (board == null || ld == null)
-      return 0
+      if (board.isActive) {
+        board.cells[row][col].isMarked = true
 
-    for ((i, row) in board.numbers.withIndex()) {
-      for ((j, num) in row.withIndex()) {
-        if (!board.isMarked[i][j]) {
-          unmarkedSum += num
+        val winnerBoardId = checkWinner(boardId, row, col)
+        winnerBoardId?.let { 
+          handleWinner(winnerBoardId = it, lastDrawn = number)
         }
       }
-    }   
-
-    return unmarkedSum * ld
-  }
-
-  fun drawNumber(number: Int) {
-    lastDrawn = number
-    numberInstances[number]?.forEach({
-      boards[it.boardId]!!.isMarked[it.row][it.column] = true
     })
   }
 
   fun preProcessBoards() {
     for ((boardId, board) in boards) {
-      for ((i, row) in board.numbers.withIndex()) {
-        for ((j, num) in row.withIndex()) {
-          numberInstances[num] = numberInstances.getOrPut(
-            num, 
-            { listOf<NumberInstance>() }
+      for ((i, row) in board.cells.withIndex()) {
+        for ((j, cell) in row.withIndex()) {
+          val empty = listOf<NumberInstance>()
+
+          numberInstances[cell.number] = numberInstances.getOrPut(
+              cell.number, 
+              { empty }
           ) + NumberInstance(boardId, i, j)
         }
       }
@@ -89,6 +89,7 @@ class Game {
           }
           break
         }
+
         "" -> {
           if (boardBuilder.isNotEmpty()) {
             buildBoard(boardBuilder, boardId)
@@ -96,6 +97,7 @@ class Game {
           }
           boardBuilder = mutableListOf<List<Int>>()
         }
+
         else -> {
           val row = line
             .split(" ")
@@ -109,15 +111,38 @@ class Game {
   }
 
   private fun buildBoard(boardBuilder: List<List<Int>>, boardId: Int) {
-    val numbers = boardBuilder
-      .map({ row -> row.toIntArray() })
+    val cells = boardBuilder
+      .map({ 
+        row -> row.map({ num -> BoardCell(num) }).toTypedArray() 
+      })
       .toTypedArray()
 
-    val nrows = numbers.size
-    val ncols = numbers[0].size
-    val isMarked = Array(nrows, { BooleanArray(ncols) })
+    boards[boardId] = Board(boardId, cells)
+  }
 
-    boards[boardId] = Board(boardId, numbers, isMarked)
+  private fun getScore(boardId: Int, lastDrawn: Int) : Int {
+    val board = boards[boardId]!!
+    var unmarkedSum = 0
+
+    for (row in board.cells) {
+      for (cell in row) {
+        if (!cell.isMarked) {
+          unmarkedSum += cell.number
+        }
+      }
+    }   
+
+    return unmarkedSum * lastDrawn
+  }
+
+  private fun handleWinner(winnerBoardId: Int, lastDrawn: Int) {
+    val winner = Winner(
+      boardId = winnerBoardId,
+      score = getScore(winnerBoardId, lastDrawn),
+    )
+
+    winners.add(winner)
+    boards[winnerBoardId]!!.isActive = false
   }
 }
 
@@ -128,7 +153,6 @@ fun readInputNumbers() : List<Int> {
 }
 
 fun main() {
-  var winnerBoardId: Int? = null
   val inputNumbers = readInputNumbers()
   val game = Game()
  
@@ -137,10 +161,10 @@ fun main() {
   
   for (number in inputNumbers) {
     game.drawNumber(number)
-    winnerBoardId = game.checkWinner()
-    if (winnerBoardId != null)
+    if (game.isOver)
       break
   }
 
-  println(game.getScore(winnerBoardId!!))
+  println("Part 1: ${game.winners.first().score}")
+  println("Part 2: ${game.winners.last().score}")
 }
